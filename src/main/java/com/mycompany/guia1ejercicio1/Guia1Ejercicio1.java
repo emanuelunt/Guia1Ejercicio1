0cdbe4e74e951086bc17c72af712d9df34a773d1
Marcelo Cano <mecano@catamarca.edu.ar>
2023-08-11 17:13:56 -0300

    Finalizado

---
+++ src/main/java/com/mycompany/guia1ejercicio1/Guia1Ejercicio1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guia1ejercicio1;
+
+/**
+ *
+ * @author Hogar
+ */
+public class Guia1Ejercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
